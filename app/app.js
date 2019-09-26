c3fd4e9dc48a6c60fa4bdd1f3813c16daf39f6a0
Jony635 <jony635molina@gmail.com>
2019-09-26 12:55:23 +0200

    Sticky navigation bar

--- app/app.js
+++ app/app.js
@@ -168,10 +168,10 @@ var MainBar = function (_React$Component3) {
         value: function render() {
             return React.createElement(
                 "div",
-                null,
+                { className: "MainBar" },
                 React.createElement(
                     "ul",
-                    { className: "MainBar" },
+                    { id: "MainBar", className: "MainBar" },
                     React.createElement(
                         "li",
                         { className: "MainBar" },
@@ -216,8 +216,31 @@ var MainBar = function (_React$Component3) {
     return MainBar;
 }(React.Component);
 
-var App = function (_React$Component4) {
-    _inherits(App, _React$Component4);
+var Image = function (_React$Component4) {
+    _inherits(Image, _React$Component4);
+
+    function Image(props) {
+        _classCallCheck(this, Image);
+
+        return _possibleConstructorReturn(this, (Image.__proto__ || Object.getPrototypeOf(Image)).call(this, props));
+    }
+
+    _createClass(Image, [{
+        key: "render",
+        value: function render() {
+            return React.createElement(
+                "div",
+                { className: "ImageContainer" },
+                React.createElement("img", { src: "resources/testImage.jpg" })
+            );
+        }
+    }]);
+
+    return Image;
+}(React.Component);
+
+var App = function (_React$Component5) {
+    _inherits(App, _React$Component5);
 
     function App(props) {
         _classCallCheck(this, App);
@@ -231,21 +254,45 @@ var App = function (_React$Component4) {
             return React.createElement(
                 "div",
                 null,
+                React.createElement(Image, null),
                 React.createElement(MainBar, null),
                 React.createElement(
                     "div",
-                    null,
+                    { id: "content" },
                     React.createElement(
-                        "div",
-                        { className: "inlineElem" },
-                        React.createElement(Clock, null),
-                        React.createElement(SwitchButton, null),
-                        React.createElement(
-                            "audio",
-                            { id: "mainAudio", loop: true },
-                            React.createElement("source", { src: "resources/Bely Basarte - Mariposas.mp3", type: "audio/mp3" }),
-                            "Your browser does not support the audio element."
-                        )
+                        "p",
+                        null,
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eget sagittis nunc. Fusce tempor lacus at odio accumsan euismod. In sem velit, dictum sit amet ultricies vitae, iaculis ac turpis. Mauris ac malesuada nisi. In aliquam ligula vel neque pretium dapibus. Praesent rhoncus nisi viverra, accumsan mi vel, pharetra magna. Fusce id fringilla mauris. Nullam convallis, lorem vel convallis tempus, metus neque tincidunt nibh, in commodo neque leo tempor orci. Mauris ex lectus, aliquam eget cursus id, finibus ac quam. Proin iaculis erat quis ligula sollicitudin, nec condimentum justo auctor. Nam vulputate nisi eu nulla convallis, eget tempor odio bibendum. Mauris id neque ac mauris tempus rhoncus. Etiam at dolor dolor. Donec facilisis non nunc eleifend sollicitudin. Proin faucibus, ipsum sed porttitor rhoncus, massa velit porttitor diam, sed dictum dui sapien vitae arcu. Cras at tellus vel magna pellentesque maximus."
+                    ),
+                    React.createElement(
+                        "p",
+                        null,
+                        "Nam et tortor tellus. Integer efficitur mollis elit, a aliquet elit dictum ut. Donec pellentesque dolor in urna bibendum, vel pellentesque nisi bibendum. Vivamus pharetra tellus nisi, in egestas diam sollicitudin id. Phasellus nec semper magna. Nullam maximus mauris non tortor vestibulum blandit. Sed elementum dolor porttitor turpis tempus pretium. Cras sodales magna nec pretium consectetur. Donec diam ipsum, consequat auctor lorem et, viverra finibus est. Etiam molestie, orci a accumsan facilisis, orci sem tempor urna, a fringilla leo orci a risus. Duis eu quam ante. Morbi quis condimentum felis."
+                    ),
+                    React.createElement(
+                        "p",
+                        null,
+                        "Donec fermentum tincidunt commodo. Praesent ultricies risus id sapien accumsan volutpat. Quisque mi orci, ultricies ac sodales sit amet, dapibus sit amet nisl. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent pretium ligula nunc, eu blandit risus ullamcorper sit amet. Donec eleifend est sit amet leo venenatis ornare. Fusce tempor mollis dolor, vel eleifend velit aliquet tempor. Vestibulum laoreet felis est, ac scelerisque sapien suscipit varius. Nunc convallis scelerisque sapien eget rhoncus. In quis tempor lectus, vitae tempus dui. Ut elementum mi ac elit accumsan, non aliquam dui venenatis. Praesent porttitor pellentesque libero in eleifend."
+                    ),
+                    React.createElement(
+                        "p",
+                        null,
+                        "Morbi faucibus ac odio eget commodo. Donec lectus purus, mollis id malesuada ut, vestibulum in lectus. Cras dictum lacus quis augue varius sagittis. Proin tristique pretium lacus, a hendrerit nisi accumsan id. Aliquam tristique tempor diam, vel dictum magna placerat a. Sed dignissim vestibulum lorem a tempor. Sed id mi non tortor faucibus volutpat. Sed quis magna dolor. Phasellus a ligula ut enim tristique tincidunt. Integer ornare eu libero a consequat."
+                    ),
+                    React.createElement(
+                        "p",
+                        null,
+                        "Donec imperdiet ut metus id commodo. Praesent eget fringilla ex. Phasellus eu molestie urna, ut tincidunt diam. Suspendisse vitae cursus est. Sed sit amet augue vitae dolor euismod lobortis. Etiam blandit augue non varius dapibus. Aliquam imperdiet pretium nibh. Mauris gravida posuere consectetur."
+                    ),
+                    React.createElement(
+                        "p",
+                        null,
+                        "Nulla accumsan elit a euismod sagittis. Suspendisse luctus auctor nibh, nec tempus nibh consectetur non. Ut hendrerit tellus sem, ut rhoncus risus tincidunt id. Duis elit ex, tempus at pretium eget, aliquet sit amet velit. Nunc sit amet lacinia leo. Nunc venenatis nisl eget ligula tincidunt vehicula. Fusce ornare quam purus, eu semper enim sodales vel. Sed sagittis urna turpis, ac molestie lorem auctor non. Mauris quis ligula luctus, pretium tellus non, faucibus quam. Fusce sit amet mauris ac ipsum ultricies mattis. Duis faucibus nisi ut sem sagittis laoreet. Fusce quis sagittis nibh. Nullam vitae nulla neque. Aliquam elementum ultricies erat sollicitudin mollis."
+                    ),
+                    React.createElement(
+                        "p",
+                        null,
+                        "Proin pulvinar elementum quam ut varius. Sed pretium urna nibh, sit amet porta metus feugiat eget. Phasellus condimentum dui felis, vitae sodales sapien tempus eget. Curabitur tincidunt ligula at leo accumsan vehicula. Morbi blandit nulla vitae est tempus tincidunt. Phasellus condimentum hendrerit metus, a convallis ante pellentesque ut. Nulla fringilla arcu eu ipsum volutpat, at sodales urna facilisis. Aliquam vestibulum, nulla et luctus laoreet, est justo euismod nunc, ac maximus erat tortor vel massa. Etiam lacinia id justo et placerat. Cras ex metus, pulvinar eu ultrices a, convallis viverra velit. Nulla blandit mauris et urna malesuada, eu placerat lectus ultrices."
                     )
                 )
             );
@@ -256,14 +303,23 @@ var App = function (_React$Component4) {
 }(React.Component);
 
 var mainAudio = undefined;
+var mainBar = undefined;
+var content = undefined;
+
 function OnPageLoaded() {
-    mainAudio = document.getElementById("mainAudio");
-    mainAudio.volume = 0.6;
-    mainAudio.play();
-    mainAudio.playing = true;
-    mainAudio.onended = function () {
-        mainAudio.play();
-    };
+    window.onscroll = OnScroll;
+    mainBar = document.getElementById("MainBar");
+    content = document.getElementById("content");
+}
+
+function OnScroll() {
+    if (window.pageYOffset >= 200) {
+        mainBar.classList.add("sticky");
+        content.classList.add("content");
+    } else {
+        mainBar.classList.remove("sticky");
+        content.classList.remove("content");
+    }
 }
 
 ReactDOM.render(React.createElement(App, null), document.getElementById("root"));
--- src/app.js
+++ src/app.js
@@ -131,19 +131,32 @@ class MainBar extends React.Component
     constructor(props)
     {
         super(props);
-
-
     }
 
     render()
     {
-        return <div>
-            <ul className = "MainBar">
+        return <div className = "MainBar">
+            <ul id = "MainBar" className = "MainBar">
                 <li className = "MainBar"><a className = "MainBar" href = "">Home</a></li>
                 <li className = "MainBar"><a className = "MainBar" href="">News</a></li>
                 <li className = "MainBar"><a className = "MainBar" href="">Contact</a></li>
                 <li className = "MainBar"><a className = "MainBar" href="">About</a></li>
             </ul>
+        </div>
+    }
+}
+
+class Image extends React.Component
+{
+    constructor(props)
+    {
+        super(props);
+    }
+
+    render()
+    {
+        return <div className = "ImageContainer">
+            <img src = "resources/testImage.jpg"></img>
         </div>
     }
 }
@@ -158,37 +171,80 @@ class App extends React.Component
     render()
     {
         return <div>
-            
+            <Image/>
             <MainBar/>
-            <div>
-                <div className = "inlineElem">
             
-                <Clock/>
-                <SwitchButton/>
-                
-                <audio id = "mainAudio" loop >
-                    <source src="resources/Bely Basarte - Mariposas.mp3" type="audio/mp3"/>
-                    Your browser does not support the audio element.
-                </audio> 
+            <div id = "content">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eget sagittis nunc. 
+                    Fusce tempor lacus at odio accumsan euismod. In sem velit, dictum sit amet ultricies vitae, iaculis ac turpis. Mauris ac malesuada nisi. 
+                    In aliquam ligula vel neque pretium dapibus. Praesent rhoncus nisi viverra, accumsan mi vel, pharetra magna. Fusce id fringilla mauris. 
+                    Nullam convallis, lorem vel convallis tempus, metus neque tincidunt nibh, in commodo neque leo tempor orci.
+                    Mauris ex lectus, aliquam eget cursus id, finibus ac quam. Proin iaculis erat quis ligula sollicitudin, nec condimentum justo auctor. 
+                    Nam vulputate nisi eu nulla convallis, eget tempor odio bibendum. Mauris id neque ac mauris tempus rhoncus. Etiam at dolor dolor. 
+                    Donec facilisis non nunc eleifend sollicitudin. Proin faucibus, ipsum sed porttitor rhoncus, massa velit porttitor diam, sed dictum dui sapien vitae arcu. 
+                    Cras at tellus vel magna pellentesque maximus.
+                </p>
 
-                </div>    
+                <p>
+                    Nam et tortor tellus. Integer efficitur mollis elit, a aliquet elit dictum ut. Donec pellentesque dolor in urna bibendum, vel pellentesque nisi bibendum. Vivamus pharetra tellus nisi, in egestas diam sollicitudin id. Phasellus nec semper magna. Nullam maximus mauris non tortor vestibulum blandit. Sed elementum dolor porttitor turpis tempus pretium. Cras sodales magna nec pretium consectetur. Donec diam ipsum, consequat auctor lorem et, viverra finibus est. Etiam molestie, orci a accumsan facilisis, orci sem tempor urna, a fringilla leo orci a risus. Duis eu quam ante. Morbi quis condimentum felis.
+                </p>
+
+                <p>
+                    Donec fermentum tincidunt commodo. Praesent ultricies risus id sapien accumsan volutpat. Quisque mi orci, ultricies ac sodales sit amet, dapibus sit amet nisl. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent pretium ligula nunc, eu blandit risus ullamcorper sit amet. Donec eleifend est sit amet leo venenatis ornare. Fusce tempor mollis dolor, vel eleifend velit aliquet tempor. Vestibulum laoreet felis est, ac scelerisque sapien suscipit varius. Nunc convallis scelerisque sapien eget rhoncus. In quis tempor lectus, vitae tempus dui. Ut elementum mi ac elit accumsan, non aliquam dui venenatis. Praesent porttitor pellentesque libero in eleifend.
+                </p>
+
+                <p>
+                    Morbi faucibus ac odio eget commodo. Donec lectus purus, mollis id malesuada ut, vestibulum in lectus. Cras dictum lacus quis augue varius sagittis. Proin tristique pretium lacus, a hendrerit nisi accumsan id. Aliquam tristique tempor diam, vel dictum magna placerat a. Sed dignissim vestibulum lorem a tempor. Sed id mi non tortor faucibus volutpat. Sed quis magna dolor. Phasellus a ligula ut enim tristique tincidunt. Integer ornare eu libero a consequat.
+                </p>
+
+                <p>
+                    Donec imperdiet ut metus id commodo. Praesent eget fringilla ex. Phasellus eu molestie urna, ut tincidunt diam. Suspendisse vitae cursus est. Sed sit amet augue vitae dolor euismod lobortis. Etiam blandit augue non varius dapibus. Aliquam imperdiet pretium nibh. Mauris gravida posuere consectetur.
+                </p>
+
+                <p>
+                    Nulla accumsan elit a euismod sagittis. Suspendisse luctus auctor nibh, nec tempus nibh consectetur non. Ut hendrerit tellus sem, ut rhoncus risus tincidunt id. Duis elit ex, tempus at pretium eget, aliquet sit amet velit. Nunc sit amet lacinia leo. Nunc venenatis nisl eget ligula tincidunt vehicula. Fusce ornare quam purus, eu semper enim sodales vel. Sed sagittis urna turpis, ac molestie lorem auctor non. Mauris quis ligula luctus, pretium tellus non, faucibus quam. Fusce sit amet mauris ac ipsum ultricies mattis. Duis faucibus nisi ut sem sagittis laoreet. Fusce quis sagittis nibh. Nullam vitae nulla neque. Aliquam elementum ultricies erat sollicitudin mollis.
+                </p>
+
+                <p>
+                    Proin pulvinar elementum quam ut varius. Sed pretium urna nibh, sit amet porta metus feugiat eget. Phasellus condimentum dui felis, vitae sodales sapien tempus eget. Curabitur tincidunt ligula at leo accumsan vehicula. Morbi blandit nulla vitae est tempus tincidunt. Phasellus condimentum hendrerit metus, a convallis ante pellentesque ut. Nulla fringilla arcu eu ipsum volutpat, at sodales urna facilisis. Aliquam vestibulum, nulla et luctus laoreet, est justo euismod nunc, ac maximus erat tortor vel massa. Etiam lacinia id justo et placerat. Cras ex metus, pulvinar eu ultrices a, convallis viverra velit. Nulla blandit mauris et urna malesuada, eu placerat lectus ultrices.              
+                </p>
+
+            
             </div>
-                  
+
+            
+            
         </div>      
     }
 }
 
 var mainAudio = undefined;
+var mainBar = undefined;
+var content = undefined
+
 function OnPageLoaded()
 {
-    mainAudio = document.getElementById("mainAudio");
-    mainAudio.volume = 0.6;
-    mainAudio.play();
-    mainAudio.playing = true;    
-    mainAudio.onended = function()
+    window.onscroll = OnScroll;
+    mainBar = document.getElementById("MainBar");
+    content = document.getElementById("content");
+}
+
+
+function OnScroll()
+{
+    if (window.pageYOffset >= 200) 
     {
-        mainAudio.play();
+        mainBar.classList.add("sticky")
+        content.classList.add("content");
+    } 
+    else 
+    {
+        mainBar.classList.remove("sticky");
+        content.classList.remove("content");
     }
 }
+
+
 
 ReactDOM.render(<App/>, document.getElementById("root"));
